c9a7b9fe6bdd75a8af5eb1ed654c6ae2bceeef07
Christian Schmidt <47923480+chrisschm@users.noreply.github.com>
2019-03-24 14:17:49 +0100

    recoded from FCards

---
+++ PlayingCard/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("PlayingCard")>
+<Assembly: AssemblyDescription("Control to paint a playing card")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PlayingCard")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("db8fd3f0-e155-466c-adfd-82ec77ad0058")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+' übernehmen, indem Sie "*" eingeben:
+
+<Assembly: AssemblyVersion("1.1.1.140")>
+<Assembly: AssemblyFileVersion("1.1.1.140")>
+<assembly: AssemblyInformationalVersion("1.1.1.140")>
